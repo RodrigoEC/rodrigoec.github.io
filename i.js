acd894f54bdf2b96788d587c0c9d470c8bfb58af
Rodrigo <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-11-02 00:33:07 -0300

    test

--- i.js
+++ i.js
@@ -1,3 +1,15 @@
-function barrinha() {
-  
+  var contador = 0;
+  var textoMaior = "o Eloy"
+  var text = "";
+  var destination = window.document.getElementById("lastName");
+function addingLetters() {
+
+  while(contador <= 6) [
+      
+      destination.innerHTML = text + textoMaior.substring(0, contador),
+      contador++,
+  ]
 }
+
+addingLetters();
+
