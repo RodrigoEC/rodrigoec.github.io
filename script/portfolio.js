0e81856843400248c9bc44316e6420b9b4635800
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-06-04 12:41:00 -0300

    Comecei e estou quase terminando a parte de responsividade do site

---
+++ script/portfolio.js
@@ -0,0 +1,20 @@
+const projectItems = document.querySelectorAll(".projects li")
+
+
+for (item of projectItems) {
+    item.addEventListener('mouseover', increaseSelectedItemSize)
+    item.addEventListener('mouseout', decreaseSelectedItemSize)
+}
+
+function increaseSelectedItemSize(event) {
+    const itemLi = event.target
+    itemLi.classList.add("selected")
+
+}
+
+function decreaseSelectedItemSize(event) {
+    const itemLi = event.target
+
+    itemLi.classList.remove("selected")
+}
+
