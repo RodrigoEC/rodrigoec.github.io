6e2547d141eed1a58a733c7839f1443cca4573ab
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-26 20:59:19 -0300

    adding footer section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Menu from './components/Menu'
 import Cabecalho from './components/cabecalho'
 import AboutMe from './components/AboutMe'
 import ThingsILove from './components/ThingsILove'
-
+import Footer from './components/Footer'
 import './assets/App.css';
 import Projects from './components/Projects/Projects';
 import { projects } from './data/projects'
@@ -23,6 +23,7 @@ function App() {
           <ThingsILove />
         </div>
       </main>
+      <Footer/>
     </div>
   );
 }
