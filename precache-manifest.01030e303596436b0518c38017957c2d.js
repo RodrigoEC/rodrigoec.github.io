ef27fd42317444cb0e5d94f07b4bf2eb105d3597
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-26 21:22:02 -0300

    Updates

---
+++ precache-manifest.01030e303596436b0518c38017957c2d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bcfbd64d405431f45a195eea5c3299",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "26a121e45c585343a3bc",
+    "url": "/portfolio/static/css/main.780b28ce.chunk.css"
+  },
+  {
+    "revision": "38dbe91d4f36d87b1f8a",
+    "url": "/portfolio/static/js/2.7deada4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.7deada4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a121e45c585343a3bc",
+    "url": "/portfolio/static/js/main.437303be.chunk.js"
+  },
+  {
+    "revision": "71039b2267fd9282f766",
+    "url": "/portfolio/static/js/runtime-main.dcf2f51d.js"
+  },
+  {
+    "revision": "9e6dd42e85422b739c0a26a7fac71cea",
+    "url": "/portfolio/static/media/dot.9e6dd42e.svg"
+  },
+  {
+    "revision": "ab6c3de2b8a166ada10df9d19da3ceb0",
+    "url": "/portfolio/static/media/eu.ab6c3de2.jpeg"
+  },
+  {
+    "revision": "d981be16cef102566c48774387837116",
+    "url": "/portfolio/static/media/github-icon-white.d981be16.svg"
+  },
+  {
+    "revision": "2977a212650aa4bc03f1392b1a11e873",
+    "url": "/portfolio/static/media/github-icon.2977a212.svg"
+  },
+  {
+    "revision": "e1cdb7a60905aaff45d6ca9e8f0aeaa4",
+    "url": "/portfolio/static/media/instagram-icon.e1cdb7a6.svg"
+  },
+  {
+    "revision": "62fcc4c14faeb3053f29c593340ad99e",
+    "url": "/portfolio/static/media/instagram-white-icon.62fcc4c1.svg"
+  },
+  {
+    "revision": "007be217db0ad2a6bebaaeb6f2d488f5",
+    "url": "/portfolio/static/media/linkedin-icon-white.007be217.svg"
+  },
+  {
+    "revision": "f87e951e5663c5e4a62e2c35b3869a76",
+    "url": "/portfolio/static/media/linkedin-icon.f87e951e.svg"
+  },
+  {
+    "revision": "8d99e79acb885ec6f8b26e33e6096539",
+    "url": "/portfolio/static/media/marimbondo.8d99e79a.jpeg"
+  },
+  {
+    "revision": "1808240d4a6097cc3b0d5960f085c5cd",
+    "url": "/portfolio/static/media/mom.1808240d.jpeg"
+  }
+]);
