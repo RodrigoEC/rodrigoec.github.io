ba412270b65af77b5d1868a08aed061ec9ea5b96
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-26 21:42:59 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.56d38e38a9a1b84a54535541bd237862.js"
+  "/portfolio/precache-manifest.01030e303596436b0518c38017957c2d.js"
 );
 
 self.addEventListener('message', (event) => {
