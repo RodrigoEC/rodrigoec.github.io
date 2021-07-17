bf18794d6f9975ff00b27615dc6f33fc2363df73
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-07-17 19:51:26 -0300

    Atualiza pagina de about

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
+
+import { ReactComponent as Heart } from '../../assets/heart.svg'
+
 import style from './style.module.css'
 
 export const About = () => {
     return (
         <div >
             <section className={style.aboutMe}>
-                <img src='https://avatars.githubusercontent.com/u/42751604?v=4' />
+                <a href='https://github.com/rodrigoec' target='_blank'>
+
+                    <img src='https://avatars.githubusercontent.com/u/42751604?v=4' />
+                </a>
                 <div className={style.text}>
                     <h1>Hi!</h1>
                     <p>
@@ -20,25 +26,28 @@ export const About = () => {
             </section>
             <section className={style.love}>
                 <div>
-                    <h1>What I love</h1>
+                    <div className={style.title}>
+                        <h1>What I love</h1>
+                        <Heart/>
+                    </div>
                     <p>
-                        As Dr. Ana Cláudia Quintana once said, “I think is sweeter to define 
+                        Quoting Dr. Ana Cláudia Quintana, “I think is sweeter to define
                         ourselves by the things we love”, therefore, who am I?
                     </p><p>
-                        I love to create new things and that translates itself in really different 
-                        areas of my life. For example, I went to Science Computer college because I 
-                        wanted to create solutions to problems of the day-to-day life. I also love 
-                        to take photographs, create the scene in my head and try to make it come to 
+                        I love to create new things and that translates itself in really different
+                        areas of my life. For example, I went to Science Computer college because I
+                        wanted to create solutions to problems of the day-to-day life. I also love
+                        to take photographs, create the scene in my head and try to make it come to
                         life is one of the things that I’m obsessed about.
                     </p><p>
-                    So..who am I? I’m just a creative person that is trying to make life a little bit easier.
+                        So..who am I? I’m just a creative person that is trying to make life a little bit easier.
                     </p>
                 </div>
                 <div>
 
                 </div>
             </section>
-            
+
 
 
         </div>
