5461c620fccfa35d4cd4e6b4f6da5aa7db50f505
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-07-18 21:01:08 -0300

    Refatora icones do componente LinkSocialMEdia

--- src/components/LinkSocialMedia/index.js
+++ src/components/LinkSocialMedia/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useTheme } from '../../context/theme.context'
 
 import style from './style.module.css'
 
 export const LinkSocialMedia = ({ info }) => {
+    const { isActive } = useTheme()
     const { title, body, Icon, link } = info
 
     return (
@@ -12,7 +14,7 @@ export const LinkSocialMedia = ({ info }) => {
                 rel='referrer'
                 target='_blank'>
 
-                <Icon className={style.icon} />
+                <Icon className={`${style.icon} ${isActive ? style.logoActive : ''}`} />
             </a>
             <div>
                 <h5 className={style.title}>{title}:</h5>
