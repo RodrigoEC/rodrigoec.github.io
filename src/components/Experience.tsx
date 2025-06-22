2b7042c58bd501584d63ac52dcbb07ad03e72007
rodrigoec <rodrigoecavalcanti@gmail.com>
2025-06-22 14:51:38 -0300

    Feat: Adds responsivity on experiences cards

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -21,7 +21,7 @@ export const Experience = ({
   const { title, position, start, end, thumb } = experience.fields;
 
   return (
-    <div className="flex flex-col md:flex-row justify-between w-full items-start md:items-center gap-2">
+    <div className="flex flex-col xl:flex-row justify-between w-full items-start md:items-center gap-2">
       <div className="flex flex-row gap-2 w-full">
         <Image
           className="grow-1 border-2 border-[black] shadow-no-expand rounded-md aspect-1/1 h-full max-h-12 w-full max-w-12"
@@ -32,7 +32,7 @@ export const Experience = ({
         />
         <div>
           <h3
-            className="text-xl font-bold w-full truncate lg:overflow-visible lg:whitespace-normal lg:text-clip"
+            className="text-xl font-bold w-full truncate xl:overflow-visible xl:whitespace-normal xl:text-clip"
             title={position}
           >
             {position}
@@ -42,8 +42,8 @@ export const Experience = ({
           </span>
         </div>
       </div>
-      <div className="flex flex-col text-md opacity-60 justify-end">
-        <div className="flex flex-col gap-2 items-end italic text-sm">
+      <div className="flex w-full xl:w-fit flex-col text-md opacity-60 justify-end">
+        <div className="flex justify-between w-full  xl:w-fit flex-row xl:flex-col gap-2 items-end italic text-sm">
           <span className="whitespace-nowrap text-start">{end ? formatDate(end) : "currently"}</span>
           <span className="whitespace-nowrap text-end">{formatDate(start)}</span>
         </div>
